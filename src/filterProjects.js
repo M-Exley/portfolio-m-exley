b52db8f04ec13199f5a501e88e13329e29f39192
Macaylen <macaylen.exley@gmail.com>
2025-11-15 16:36:34 +0100

    Leaving for the moment

---
+++ src/filterProjects.js
@@ -0,0 +1,11 @@
+export default function filterProjects() {
+  const container = document.querySelector(".clickable-languages");
+  let selection;
+
+  container.addEventListener("click", (e) => {
+    if (e.target.tagName === "P") {
+      selection = e.target.textContent.toLowerCase();
+      console.log(selection);
+    }
+  });
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import "./style.css";
 
+import filterProjects from "./filterProjects";
+
 window.addEventListener("scroll", () => {
   const header = document.querySelector(".svg");
   header.style.opacity = window.scrollY > 100 ? "0" : "1";
 });
+
+filterProjects();
